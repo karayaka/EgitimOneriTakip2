eaa669a1a475c15fd53f9105a98c39443bdd6798
Teknik <Teknik@B014>
2019-03-22 21:40:22 +0300

    labeller eklendi

--- EgitimOneriTakip/frmKullaniciGiris.Designer.cs
+++ EgitimOneriTakip/frmKullaniciGiris.Designer.cs
@@ -28,11 +28,43 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+			this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+			this.SuspendLayout();
+			// 
+			// labelControl1
+			// 
+			this.labelControl1.Location = new System.Drawing.Point(32, 24);
+			this.labelControl1.Name = "labelControl1";
+			this.labelControl1.Size = new System.Drawing.Size(63, 13);
+			this.labelControl1.TabIndex = 0;
+			this.labelControl1.Text = "labelControl1";
+			// 
+			// labelControl2
+			// 
+			this.labelControl2.Location = new System.Drawing.Point(32, 43);
+			this.labelControl2.Name = "labelControl2";
+			this.labelControl2.Size = new System.Drawing.Size(63, 13);
+			this.labelControl2.TabIndex = 1;
+			this.labelControl2.Text = "labelControl2";
+			// 
+			// frmKullaniciGiris
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(385, 249);
+			this.Controls.Add(this.labelControl2);
+			this.Controls.Add(this.labelControl1);
+			this.Name = "frmKullaniciGiris";
 			this.Text = "frmKullaniciGiris";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private DevExpress.XtraEditors.LabelControl labelControl1;
+		private DevExpress.XtraEditors.LabelControl labelControl2;
 	}
 }
